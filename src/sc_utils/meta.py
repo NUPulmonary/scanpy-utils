3264b4541f4fca4acddfb8a3a49cc4bc8963d32d
Nikolay Markov <nikolai.markov@icloud.com>
2021-06-24 14:07:35 -0500

    Fix regex strings

--- src/sc_utils/meta.py
+++ src/sc_utils/meta.py
@@ -1,7 +1,7 @@
 
 
 def clean_metadata(ds):
-    x = ds.var.loc[:, ds.var.columns[ds.var.columns.str.match("gene_ids-\d+")]]
+    x = ds.var.loc[:, ds.var.columns[ds.var.columns.str.match(r"gene_ids-\d+")]]
     cols = x.T.notna().idxmax()
     x = x.reset_index().melt("index")
     ds.var.insert(
@@ -9,10 +9,10 @@ def clean_metadata(ds):
         "gene_ids",
         x.set_index(["index", "variable"]).loc[zip(cols.index, cols.values), :].droplevel("variable")
     )
-    ds.var.drop(ds.var.columns[ds.var.columns.str.match("gene_ids-\d+")], inplace=True, axis=1)
+    ds.var.drop(ds.var.columns[ds.var.columns.str.match(r"gene_ids-\d+")], inplace=True, axis=1)
     ds.var.insert(1, "feature_types", "Gene Expression")
-    ds.var.drop(ds.var.columns[ds.var.columns.str.match("feature_types-\d+")], inplace=True, axis=1)
-    x = ds.var.loc[:, ds.var.columns[ds.var.columns.str.match("genome-\d+")]]
+    ds.var.drop(ds.var.columns[ds.var.columns.str.match(r"feature_types-\d+")], inplace=True, axis=1)
+    x = ds.var.loc[:, ds.var.columns[ds.var.columns.str.match(r"genome-\d+")]]
     cols = x.T.notna().idxmax()
     x = x.reset_index().melt("index")
     ds.var.insert(
@@ -20,4 +20,4 @@ def clean_metadata(ds):
         "genome",
         x.set_index(["index", "variable"]).loc[zip(cols.index, cols.values), :].droplevel("variable")
     )
-    ds.var.drop(ds.var.columns[ds.var.columns.str.match("genome-\d+")], inplace=True, axis=1)
+    ds.var.drop(ds.var.columns[ds.var.columns.str.match(r"genome-\d+")], inplace=True, axis=1)
