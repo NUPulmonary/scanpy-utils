71fd78784ea725151a5e7ea516f401b069ebede6
Nikolay Markov <nikolai.markov@icloud.com>
2021-06-24 16:09:01 -0500

    Prettify imports

--- src/sc_utils/__init__.py
+++ src/sc_utils/__init__.py
@@ -3,4 +3,6 @@ __version__ = '0.1'
 from .io import write_mtx
 from .markers import get_markers
 from .meta import clean_metadata
-from .plotting import expr_colormap, feature_plot, plot_composition
+from .plotting import expr_colormap
+from .plotting import feature_plot
+from .plotting import plot_composition
