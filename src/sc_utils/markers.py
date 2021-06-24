6486157940884cd3e0a3f9f292a551ab0f9b59e6
Nikolay Markov <nikolai.markov@icloud.com>
2021-06-24 14:07:18 -0500

    Fix adata reference

--- src/sc_utils/markers.py
+++ src/sc_utils/markers.py
@@ -10,9 +10,9 @@ def get_markers(
     logfc_cutoff=0.5
 ):
     markers = pd.concat([
-        pd.DataFrame(anndata.uns[key]["names"]).melt(),
-        pd.DataFrame(anndata.uns[key]["pvals_adj"]).melt(),
-        pd.DataFrame(anndata.uns[key]["logfoldchanges"]).melt()
+        pd.DataFrame(adata.uns[key]["names"]).melt(),
+        pd.DataFrame(adata.uns[key]["pvals_adj"]).melt(),
+        pd.DataFrame(adata.uns[key]["logfoldchanges"]).melt()
     ], axis=1)
     markers.columns = ("cluster", "gene", "cluster2", "p_val_adj", "cluster3", "avg_logFC")
     markers = markers.loc[:, ["cluster", "gene", "avg_logFC", "p_val_adj"]]
@@ -20,13 +20,13 @@ def get_markers(
     markers = markers.loc[markers.p_val_adj < p_val_cutoff, ]
 
     for cluster in markers.cluster.unique():
-        cells = anndata.obs[groupby] == cluster
+        cells = adata.obs[groupby] == cluster
         in_cluster_selector = markers.cluster == cluster
         genes = markers.gene[in_cluster_selector]
-        in_cluster = np.sum(anndata.raw[cells, genes].X > 0, axis=0) / cells.sum()
+        in_cluster = np.sum(adata.raw[cells, genes].X > 0, axis=0) / cells.sum()
         markers.loc[in_cluster_selector, "pct.1"] = in_cluster.T
-        other_cells = anndata.obs[groupby] != cluster
-        other_clusters = np.sum(anndata.raw[other_cells, genes].X > 0, axis=0) / other_cells.sum()
+        other_cells = adata.obs[groupby] != cluster
+        other_clusters = np.sum(adata.raw[other_cells, genes].X > 0, axis=0) / other_cells.sum()
         markers.loc[in_cluster_selector, "pct.2"] = other_clusters.T
 
     markers["p_val"] = markers.p_val_adj
